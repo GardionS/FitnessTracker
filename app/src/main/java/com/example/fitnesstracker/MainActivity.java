5eebad7b876e14e5a7d636d99914a877d101a255
GardionS <kanbeisensei@gmail.com>
2022-11-02 16:10:21 +0800

    v0.1
    Add Bottom Navigation

--- app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -7,12 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import com.example.fitnesstracker.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
     }
+
 
     public void goToMainLayoutButton(View view){
         setContentView(R.layout.main_screen);
--- app/src/main/java/com/example/fitnesstracker/MainScreen.java
+++ app/src/main/java/com/example/fitnesstracker/MainScreen.java
@@ -1,20 +1,41 @@
 package com.example.fitnesstracker;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainScreen extends AppCompatActivity {
+import com.example.fitnesstracker.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainScreen extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+    ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemReselectedListener((NavigationBarView.OnItemReselectedListener) this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        }
+        HomeFragment homeFragment = new HomeFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,homeFragment).commit();
+                return true;
+        }
+        return false;
     }
 
-    public void goToMainLayoutButton(View view){
-        setContentView(R.layout.main_screen);
-    }
 }
