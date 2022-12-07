3fe178810444a691ee24a689a5d3ee36c38d0f7d
GardionS <kanbeisensei@gmail.com>
2022-12-08 05:36:25 +0800

    Complete profile page
    Add function for the profile page

---
+++ app/src/main/java/com/example/fitnesstracker/ChangePasswordActivity.java
@@ -0,0 +1,53 @@
+package com.example.fitnesstracker;
+
+import static com.example.fitnesstracker.LoginActivity.ID_KEY;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+    EditText passwordOld, passwordNew;
+    Button changePassword;
+    ValidateText validateText;
+    DatabaseUser databaseUser;
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_change_password);
+        initVariable();
+    }
+    private void initVariable() {
+        sharedPreferences = getSharedPreferences(LoginActivity.SHARED_PREFS, Context.MODE_PRIVATE);
+        databaseUser = new DatabaseUser(this);
+        validateText = new ValidateText(this);
+        passwordOld = findViewById(R.id.editOldPassword);
+        passwordNew = findViewById(R.id.editNewPassword);
+        changePassword = findViewById(R.id.changePassword);
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPassword();
+            }
+        });
+    }
+    private void  checkPassword() {
+        if(!validateText.validateEditText(passwordOld, getString(R.string.error_message_password_old))
+        ||!validateText.validateEditText(passwordNew, getString(R.string.error_message_password_new))) {
+            if(databaseUser.comparePassword(sharedPreferences.getInt(ID_KEY, 0), passwordOld.getText().toString())) {
+                databaseUser.insertNewPassword(sharedPreferences.getInt(ID_KEY, 0), passwordNew.getText().toString());
+            }
+        }
+    }
+    
+}
--- app/src/main/java/com/example/fitnesstracker/HistoryFragment.java
+++ app/src/main/java/com/example/fitnesstracker/HistoryFragment.java
@@ -1,20 +1,68 @@
 package com.example.fitnesstracker;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HistoryFragment extends Fragment {
+    private FragmentActivity fragment = getActivity();
+    private DatabaseFitness databaseFitness;
+    private TextView historyCaloriesWalking, historyStepsWalking, historyDate;
+    private SharedPreferences sharedPreferences;
+
+    public HistoryFragment(DatabaseFitness databaseFitness) {
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_history, container, false);
     }
-    public void leftSlide(View view) {
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initVariable();
+        initValue();
+    }
+
+    private void initVariable() {
+//        this.databaseFitness = new DatabaseFitness(fragment);
+        historyCaloriesWalking = getView().findViewById(R.id.historyCaloriesWalking);
+        historyStepsWalking = getView().findViewById(R.id.historyStepsWalking);
+        historyDate = getView().findViewById(R.id.historyDate);
+    }
+    private void initValue(){
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = new Date();
+        Fitness fitnessToday = databaseFitness.getFitness(sharedPreferences.getInt(LoginActivity.ID_KEY, 0), format.format(date));
+        historyStepsWalking.setText(Integer.toString(fitnessToday.getWalk()));
+        historyCaloriesWalking.setText(Integer.toString(convertStepsCalorie(fitnessToday.getWalk())));
+        format = new SimpleDateFormat("E MMM dd yyyy");
+        historyDate.setText(format.format(date));
+    }
+    private int convertStepsCalorie(int steps) {
+        return (int) Math.round(steps * 0.4);
+    }
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        databaseFitness = new DatabaseFitness(getActivity());
+        sharedPreferences =  getActivity().getApplicationContext().getSharedPreferences(LoginActivity.SHARED_PREFS, Context.MODE_PRIVATE);
     }
 }
